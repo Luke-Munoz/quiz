a498761e2c1c227d979a9d76acaa7d1440da07ce
Luke <munozluke1@gmail.com>
2021-08-17 13:28:39 -0600

    is it linked? gotta turn in the assignment to move on

---
+++ assets/scripts.js
@@ -0,0 +1,123 @@
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
+var startBtn = document.getElementById("start");
+var score = 0;
+var answerLog = [];
+var questionIndex = 0;
+var timeLeft = 50;
+var timeInterval;
+
+
+var questions = [
+    { q: "What does 'for' do in javaScript?", correct: "It loops through a function.", wrongA: "it insterts HTML.", wrongB: "it does nothing. ", wrongC: "it displays in the console" },
+    { q: "Inside which HTML element do we put the JavaScript?", correct: "<script>", wrongA: "<javascript>", wrongB: "<js>", wrongC: "<scripting>" },
+    { q: "What does HTML stand for?", correct: "Hyper Text Markup Language", wrongA: "Home tool Markup Language", wrongB: "HyperLinks and text markup language", wrongC: "Hype tone manuel language" },
+    { q: "What does CSS Stand for?", correct: "", wrongA: "", wrongB: "", wrongC: "" }
+];
+
+
+
+function countdown() {
+
+
+
+
+
+    timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + "secounds remaining";
+
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + "secounds remaining";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + "secounds remaining";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Game Over";
+            clearInterval(timeInterval);
+
+        }
+    }, 1000);
+
+
+
+    gameQuestions();
+
+
+
+};
+
+console.log(countdown);
+startBtn.onclick = countdown;
+// var div1 = document.createElement(div1);
+
+// var buttonEl = document.createElement(buttonEl);
+// function newHTML() {
+//     var div1 = document.createElement(div1);
+//     var buttonEl = document.createElement(buttonEl);
+
+// };
+// document.appendChild(newHTML);
+// console.log(newHTML);
+
+
+
+
+function gameQuestions() {
+
+    mainEl.innerHTML = "";
+
+    if (questionIndex < questions.length) {
+
+
+        var newH1El = document.createElement("h1");
+        newH1El.innerText = questions[questionIndex].q;
+        mainEl.append(newH1El);
+        var newBtnEl = document.createElement("button");
+        newBtnEl.innerText = questions[questionIndex].correct;
+        mainEl.append(newBtnEl);
+
+        var btn1El = document.createElement("button");
+        btn1El.innerText = questions[questionIndex].wrongA;
+        mainEl.append(btn1El);
+
+        var btn2El = document.createElement("button");
+        btn2El.innerText = questions[questionIndex].wrongB;
+        mainEl.append(btn2El)
+
+        var btn3El = document.createElement("button");
+        btn3El.innerText = questions[questionIndex].wrongC;
+        mainEl.append(btn3El)
+    } else {
+
+        clearInterval(timeInterval);
+    }
+
+
+
+
+};
+
+function highScore() {
+
+}
+
+function answerClick(event) {
+    if (event.target.localName === "button") {
+        if (event.target.textContent === questions[questionIndex].correct) {
+
+            score++;
+        } else {
+            timeLeft = timeLeft - 10;
+            score--;
+        }
+        questionIndex++;
+        gameQuestions();
+        console.log(score);
+    }
+
+};
+
+mainEl.onclick = answerClick;
